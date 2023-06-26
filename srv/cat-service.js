8a0db105370f6cb689270a20c49dbc73658d75e4
RAGHAVENDRA REDDY DEVIREDDY <drvrdevireddy@gmail.com>
2023-06-26 12:31:40 +0000

    removed hook method

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,13 +1,5 @@
 const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function(){
-    this.after('READ','Books',req => {
-        for(var i=0;i<req.length;i++){
-            if(req[i].stock <= 250){
-                req[i].status = 1;
-            }else{
-                req[i].status = 3;
-            }
-        }
-    })
+   
 });
